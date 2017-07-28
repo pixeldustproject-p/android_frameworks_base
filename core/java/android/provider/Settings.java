9de503e13fdd6f3e841f21c31a0d25c0e8389c04
Ivan Iskandar <ivan@prjkt.io>
2017-07-28 04:56:45 +0200

    base: Introduce SubstratumService
    Change-Id: I8c9f331ca7e4c68c35ea13bed587775ee4abeb30
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>
    Signed-off-by: Nicholas Chum <nicholas@prjkt.io>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10367,6 +10367,13 @@ public final class Settings {
         public static final String SYSUI_ROUNDED_FWVALS = "sysui_rounded_fwvals";
 
         /**
+         * Force authorize Substratum (or equivalent) frontend calling packages by ThemeInterfacer
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
