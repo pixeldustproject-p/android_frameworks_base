c25035b4ceac0af2a57f825b685c4ca6cec92147
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Change-Id: I35676a16f6ee4675de4892c1ce47358dc23ada3e
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4391,6 +4391,13 @@ public final class Settings {
         public static final String POWERMENU_LOGOUT = "powermenu_logout";
 
         /**
+         * Whether to display the torch option in the power menu
+         *
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
          * Pie toggles torch
          * @hide
          */
