c4d72ff815930f7dadc69182c6b8834746064fb6
varund7726 <date.varun123@gmail.com>
2018-11-07 13:23:32 -0700

    Pulse: allow setting fading blocks opacity [1/3]
     Please enter the commit message for your changes. Lines starting
    
    Change-Id: I7c4a0aa41672a6c6ed37888c7745f91383662360
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10139,6 +10139,12 @@ public final class Settings {
         public static final String FLING_PULSE_SMOOTHING_ENABLED = "fling_pulse_smoothing_enabled";
 
         /**
+         * Pulse Fading units opacity
+         * @hide
+         */
+        public static final String PULSE_FADING_BLOCKS_OPACITY = "pulse_fading_blocks_opacity";
+
+        /**
          * Whether to enable double tap to sleep for smartbar
          * @hide
          */
