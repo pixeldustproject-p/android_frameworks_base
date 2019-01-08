4ca76e60fb7276193ab2e264c054b5e0a435f0ac
spezi77 <spezi7713@gmx.net>
2019-01-08 23:41:47 +0100

    Revert "WeatherClient: Improvements".
    Reason: This will break the weather info shown in Launcher/quickspace.
    
    Revert "WeatherClient: Add back isAvailable method"
    
    This reverts commit b5c778c959efb6b41d2589da91eab0a06f5d4d3d.
    
    Revert "WeatherClient: Improve client initiation after boot"
    
    This reverts commit 03e6aa2043fa8df077370c155661b9ff5a683480.

--- core/java/com/android/internal/util/weather/WeatherClient.java
+++ core/java/com/android/internal/util/weather/WeatherClient.java
@@ -60,8 +60,6 @@ public class WeatherClient {
             COLUMN_TEMPERATURE_IMPERIAL
     };
 
-    private boolean mBootAndUnlocked;
-
     private static final int WEATHER_UPDATE_INTERVAL = 60 * 20 * 1000; // 20 minutes
     private String updateIntentAction;
     private PendingIntent pendingWeatherUpdate;
@@ -84,14 +82,11 @@ public class WeatherClient {
                 onScreenOff();
             } else if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
                 onScreenOn();
-            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-                mBootAndUnlocked = true;
-                updateWeatherAndNotify(false);
-            } else if (updateIntentAction.equals(intent.getAction())) {
-                updateWeatherAndNotify(false);
-            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction())
-                    || Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
-                updateWeatherAndNotify(true);
+            } else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) || updateIntentAction.equals(intent.getAction())) {
+                updateWeatherAndNotify();
+            } else if (Intent.ACTION_TIME_CHANGED.equals(intent.getAction()) || Intent.ACTION_TIMEZONE_CHANGED.equals(intent.getAction())) {
+                resetScheduledAlarm();
+                updateWeatherAndNotify();
             }
         }
     };
@@ -130,10 +125,8 @@ public class WeatherClient {
         return r.nextInt((20000000 - 10000000) + 1) + 10000000;
     }
 
-    private void updateWeatherAndNotify(boolean forceResetSchedule) {
-        if (!mBootAndUnlocked) return;
-        if (isRunning) {
-            if (forceResetSchedule) resetScheduledAlarm();
+    private void updateWeatherAndNotify() {
+        if (isRunning){
             return;
         }
         isRunning = true;
@@ -161,7 +154,7 @@ public class WeatherClient {
     }
 
     private void onScreenOn() {
-        if (!mBootAndUnlocked || isScreenOn){
+        if (isScreenOn){
             return;
         }
         if (DEBUG) Log.d(TAG, "onScreenOn");
@@ -169,7 +162,7 @@ public class WeatherClient {
         if (!isRunning) {
             if (needsUpdate()) {
                 if (DEBUG) Log.d(TAG, "Needs update, triggering updateWeatherAndNotify");
-                updateWeatherAndNotify(false);
+                updateWeatherAndNotify();
             } else {
                 if (DEBUG) Log.d(TAG, "Scheduling update");
                 scheduleWeatherUpdateAlarm();
@@ -237,6 +230,22 @@ public class WeatherClient {
 
     public void addObserver(final WeatherObserver observer) {
         mObserver.add(observer);
+        if (isRunning) {
+            return;
+        }
+        isRunning = true;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                updateWeatherData();
+                try {
+                    observer.onWeatherUpdated(mWeatherInfo);
+                } catch (Exception ignored) {
+                }
+            }
+        });
+        thread.setPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
     }
 
     public void removeObserver(WeatherObserver observer) {
