c70ef4af21d3a2122ee6d9930cacc14e16218e58
spezi77 <spezi7713@gmx.net>
2019-02-21 23:43:57 +0100

    PWM: Fix navbar key event source.
    Change-Id: Ied6836d8c2161ea86baf98aa782f01ebba3cb88a
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6701,7 +6701,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         final boolean isInjected = (policyFlags & WindowManagerPolicy.FLAG_INJECTED) != 0;
 
         final int source = event.getSource();
-        final boolean navBarKey = source == InputDevice.SOURCE_NAVIGATION_BAR;
+        final boolean navBarKey = source == InputDevice.SOURCE_KEYBOARD;
         final boolean appSwitchKey = keyCode == KeyEvent.KEYCODE_APP_SWITCH;
         final boolean homeKey = keyCode == KeyEvent.KEYCODE_HOME;
         final boolean menuKey = keyCode == KeyEvent.KEYCODE_MENU;
